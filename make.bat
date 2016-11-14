0ffd57799f65616e53523cfbf9641ba74443ee2c
determin1st <baklanoid@gmail.com>
2016-11-14 15:20:17 +0500

    batch to translations\

--- make.bat
+++ make.bat
@@ -4,17 +4,20 @@
 set MK=D:\usr\x\Qt\Qt5.7.0\5.7\msvc2013\bin
 
 :: translations .ts
-set RL=D:\usr\Lab\TeamSpeak3Ru\
+set RL=%~dp0\
 
 :: ts3
-set TS=D:\usr\x\ts3client\translations\
+set TS=%~dp0translations\
 
 :: files to go
 set F1=lagos_ru
 set F2=error_report_ru
 cd %MK%
+
 lrelease "%RL%%F1%.ts" -qm "%RL%%F1%.qm"
 copy /Y "%RL%%F1%.qm" "%TS%"
 lrelease "%RL%%F2%.ts" -qm "%RL%%F2%.qm"
 copy /Y "%RL%%F2%.qm" "%TS%"
+
 exit
+
