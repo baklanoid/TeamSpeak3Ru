6a4e74394f7eaaebb028e0220d3ba23352991953
Michael Quad <baklanoid@gmail.com>
2016-11-14 15:12:13 +0500

    batch

--- make.bat
+++ make.bat
@@ -11,7 +11,10 @@ set TS=D:\usr\x\ts3client\translations\
 
 :: files to go
 set F1=lagos_ru
+set F2=error_report_ru
 cd %MK%
 lrelease "%RL%%F1%.ts" -qm "%RL%%F1%.qm"
 copy /Y "%RL%%F1%.qm" "%TS%"
+lrelease "%RL%%F2%.ts" -qm "%RL%%F2%.qm"
+copy /Y "%RL%%F2%.qm" "%TS%"
 exit
