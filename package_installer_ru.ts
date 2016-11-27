58948eaa3131760dd796f441026a350cb5d30bf5
determin1st <baklanoid@gmail.com>
2016-11-28 02:12:30 +0500

    100%

--- package_installer_ru.ts
+++ package_installer_ru.ts
@@ -5,117 +5,119 @@
     <name>InstallDialog</name>
     <message>
         <source>TeamSpeak Add-On</source>
-        <translation type="unfinished"></translation>
+        <translation>Расширение TeamSpeak</translation>
     </message>
     <message>
         <source>Name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Имя:</translation>
     </message>
     <message>
         <source>Type:</source>
-        <translation type="unfinished"></translation>
+        <translation>Тип:</translation>
     </message>
     <message>
         <source>Author:</source>
-        <translation type="unfinished"></translation>
+        <translation>Автор:</translation>
     </message>
     <message>
         <source>Version:</source>
-        <translation type="unfinished"></translation>
+        <translation>Версия:</translation>
     </message>
     <message>
         <source>Description:</source>
-        <translation type="unfinished"></translation>
+        <translation>Описание:</translation>
     </message>
     <message>
         <source>Platform:</source>
-        <translation type="unfinished"></translation>
+        <translation>Платформа:</translation>
     </message>
     <message>
         <source>This add-on is not available for your platform.</source>
-        <translation type="unfinished"></translation>
+        <translation>Это расширение не доступно на вашей платформе.</translation>
     </message>
     <message>
         <source>Install</source>
-        <translation type="unfinished"></translation>
+        <translation>Установить</translation>
     </message>
     <message>
         <source>Abort</source>
-        <translation type="unfinished"></translation>
+        <translation>Прервать</translation>
     </message>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка</translation>
     </message>
     <message>
         <source>Failed to read Add-On info.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка чтения информации о расширении</translation>
     </message>
     <message>
         <source>All</source>
-        <translation type="unfinished"></translation>
+        <translation>Все</translation>
     </message>
     <message>
         <source>Select TeamSpeak Client installation folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Выберите установочный каталог клиента TeamSpeak</translation>
     </message>
     <message>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Предупреждение</translation>
     </message>
     <message>
         <source>&lt;p&gt;You are about to install a Plugin for your TeamSpeak Client.&lt;/p&gt;&lt;p&gt;Plugins should only be installed from trusted sources, as it is possible to inject malicious code into the TeamSpeak client with a plugin.&lt;/p&gt;&lt;p&gt;Do you trust this Plugin and want to proceed installing?&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Вы собираетесь установить Плагин для клиента TeamSpeak.&lt;/p&gt;&lt;p&gt;Плагины следует устанавливать из доверенных источников, так как возможно внедрение зловредного кода в клиент, через Плагин.&lt;/p&gt;&lt;p&gt;Вы доверяете автору Плагина?&lt;/p&gt;</translation>
     </message>
     <message>
         <source>No TeamSpeak 3 Client installation directory found.</source>
-        <translation type="unfinished"></translation>
+        <translation>Установочный каталог TeamSpeak не определен.</translation>
     </message>
     <message>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Информация</translation>
     </message>
     <message>
         <source>&lt;p&gt;Add-On successfully installed.&lt;br&gt;Do you want to activate this Add-On?&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Please restart your TeamSpeak Client.&lt;/strong&gt;&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Расширение установлено.&lt;br&gt;Включить данное расширение?&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Перезапустите ваш клиент TeamSpeak.&lt;/strong&gt;&lt;/p&gt;</translation>
     </message>
     <message>
         <source>Add-On successfully installed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Расширение успешно установлено.</translation>
     </message>
     <message>
         <source>Failed to install Add-On.
 Do you want to retry as Administrator?</source>
-        <translation type="unfinished"></translation>
+        <translation>Проблема установки расширения.
+Попробовать от имени Администратора?</translation>
     </message>
     <message>
         <source>Failed to install Add-On.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не получилось установить расширение.</translation>
     </message>
     <message>
         <source>File not found: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Файл не найден: %1</translation>
     </message>
     <message>
         <source>Invalid zip file: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Неверный архив zip: %1</translation>
     </message>
     <message>
         <source>Failed to find temp directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Не удалось определить временный каталог</translation>
     </message>
     <message>
         <source>Failed to create temp path: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Не удалось создать временный каталог: %1</translation>
     </message>
     <message>
         <source>Failed to extract: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Не удалось извлечь архив: %1</translation>
     </message>
     <message>
         <source>Failed to extract Add-On:
 %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Не удалось извлечь расширение:
+%1</translation>
     </message>
 </context>
 </TS>
