6a8f18af2799d0c029a4ce54b1a4828806b2aba7
determin1st <baklanoid@gmail.com>
2016-12-20 05:20:01 +0500

    permissions_ru

--- make.bat
+++ make.bat
@@ -12,10 +12,12 @@ set TS=%~dp0translations\
 :: files to go
 set F1=lagos_ru
 set F2=error_report_ru
+set F3=permissions_ru
 cd %MK%
 
 lrelease "%RL%%F1%.ts" -qm "%TS%%F1%.qm"
 lrelease "%RL%%F2%.ts" -qm "%TS%%F2%.qm"
+lrelease "%RL%%F3%.ts" -qm "%TS%%F3%.qm"
 
 exit
 
