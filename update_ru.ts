0c82c4655068a88ef10483cbe34cb1c14f116f68
determin1st <baklanoid@gmail.com>
2016-12-23 21:20:50 +0500

    100%

--- update_ru.ts
+++ update_ru.ts
@@ -5,124 +5,124 @@
     <name>MainWindow</name>
     <message>
         <source>TeamSpeak 3 Client Update</source>
-        <translation type="unfinished"></translation>
+        <translation>Обновление клиента TeamSpeak 3</translation>
     </message>
     <message>
         <source>Details</source>
-        <translation type="unfinished"></translation>
+        <translation>Информация</translation>
     </message>
     <message>
         <source>Stable</source>
-        <translation type="unfinished"></translation>
+        <translation>Стабильные версии</translation>
     </message>
     <message>
         <source>Beta</source>
-        <translation type="unfinished"></translation>
+        <translation>Бета-версии</translation>
     </message>
     <message>
         <source>Alpha</source>
-        <translation type="unfinished"></translation>
+        <translation>Альфа-версии</translation>
     </message>
     <message>
         <source>Update to TeamSpeak %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Обновление TeamSpeak %1</translation>
     </message>
     <message>
         <source>You are currently on &lt;strong&gt;%1 channel&lt;/strong&gt;.</source>
-        <translation type="unfinished"></translation>
+        <translation>Текущий канал обновлений: &lt;strong&gt;%1&lt;/strong&gt;.</translation>
     </message>
     <message>
         <source>Step one: Updating updater</source>
-        <translation type="unfinished"></translation>
+        <translation>Шаг 1: сервисное обновление (установщика)</translation>
     </message>
     <message>
         <source>Step two: Updating TeamSpeak client</source>
-        <translation type="unfinished"></translation>
+        <translation>Шаг 2: обновление клиента TeamSpeak</translation>
     </message>
     <message>
         <source>Confirm reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>Подтвердите перезагрузку</translation>
     </message>
     <message>
         <source>&lt;p&gt;A computer restart is required to proceed with the client update. If you do not reboot now, the TeamSpeak update will be cancelled.&lt;/p&gt;&lt;p&gt;Do you want to reboot now?&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Для выполнения обновления клиента требуется перезагрузка компьютера. Если не сделать этого сейчас, обновление будет отменено.&lt;/p&gt;&lt;p&gt;Do you want to reboot now?&lt;/p&gt;</translation>
     </message>
     <message>
         <source>Question</source>
-        <translation type="unfinished"></translation>
+        <translation>Вопрос</translation>
     </message>
     <message>
         <source>&lt;p&gt;Update in progress.&lt;/p&gt;&lt;p&gt;Are you sure you want to abort?&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Идет обновление..&lt;/p&gt;&lt;p&gt;Прервать?&lt;/p&gt;</translation>
     </message>
     <message>
         <source>Waiting until TeamSpeak 3 Client has been closed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ожидаем закрытия клиента TeamSpeak.</translation>
     </message>
     <message>
         <source>Warning</source>
-        <translation type="unfinished"></translation>
+        <translation>Предупреждение</translation>
     </message>
     <message>
         <source>The TeamSpeak 3 Client should be
 closed before starting the update.</source>
-        <translation type="unfinished"></translation>
+        <translation>Чтобы приступить к обновлению, клиент TeamSpeak необходимо закрыть.</translation>
     </message>
     <message>
         <source>Information</source>
-        <translation type="unfinished"></translation>
+        <translation>Инфо</translation>
     </message>
     <message>
         <source>&lt;p&gt;This is the last TeamSpeak version available for Windows XP.&lt;/p&gt;&lt;p&gt;To upgrade TeamSpeak and take advantage of the latest features and bug fixes, please update your operating system to Windows Vista or newer.&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Это последняя версия клиента с поддержкой Windows XP.&lt;/p&gt;&lt;p&gt;Чтобы получить преимущества новых версий TeamSpeak, пожалуйста, обновите Вашу ОС до Windows Vista или выше.&lt;/p&gt;</translation>
     </message>
     <message>
         <source>%1 KB / %2 KB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 КБ / %2 КБ</translation>
     </message>
     <message>
         <source>%1 KB/s</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 КБ/с</translation>
     </message>
     <message>
         <source>Update cancelled</source>
-        <translation type="unfinished"></translation>
+        <translation>Обновление отменено</translation>
     </message>
     <message>
         <source>Update</source>
-        <translation type="unfinished"></translation>
+        <translation>Обновление</translation>
     </message>
     <message>
         <source>Update cancelled. Push &quot;Details&quot; to check reasons.</source>
-        <translation type="unfinished"></translation>
+        <translation>Обновление отменено. Нажмите кнопку &quot;Details&quot; для получения дополнительных сведений.</translation>
     </message>
     <message>
         <source>Click to open &quot;%1://%2&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Кликни чтобы открыть &quot;%1://%2&quot;</translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка</translation>
     </message>
     <message>
         <source>Failed to query latest client
 version from update server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не удалось получить номер последней версии с сервера обновлений.</translation>
     </message>
     <message>
         <source>Update server returned no version</source>
-        <translation type="unfinished"></translation>
+        <translation>Сервер обновлений ничего не ответил</translation>
     </message>
     <message>
         <source>Update server returned invalid version nr (%1)</source>
-        <translation type="unfinished"></translation>
+        <translation>Сервер обновлений вернул неверное значение версии (%1)</translation>
     </message>
     <message>
         <source>Invalid version nr (%1)</source>
-        <translation type="unfinished"></translation>
+        <translation>Неверное значение версии (%1)</translation>
     </message>
 </context>
 </TS>
